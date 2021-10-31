894c967417a709021ea743f9133e2604c609cd47
Frida-abella <77575398+Frida-abella@users.noreply.github.com>
2021-10-31 23:09:08 +0100

    comentarios clase libro

--- Act2_Sockets_J/src/SocketServidor/Libro.java
+++ Act2_Sockets_J/src/SocketServidor/Libro.java
@@ -1,12 +1,17 @@
 package SocketServidor;
 
+// Creamos una clase LIBRO que sirva para crear objetos de este tipo, con los diferentes datos 
+
 public class Libro {
 	
+	// Declaramos los atributos que componen el objeto, que permitirán al usuario buscar al objeto o añadir uno nuevo 
 	private String isbn;
 	private String autor;
 	private String titulo;
 	private String precio;
 	
+	// Creamos el método constructor del objeto Libro, al que se le pasarán como parámetros los Strings con el isbn, autor, titulo y precio,
+	// Para que pasen a ser los atributos de ese nuevo objeto:
 	public Libro(String isbn, String autor, String titulo, String precio) {
 		super();
 		this.isbn = isbn;
@@ -19,7 +24,8 @@ public class Libro {
 	public Libro() {
 		
 	}
-
+	
+	// Creamos los Getter & Setter
 	public String getIsbn() {
 		return isbn;
 	}
@@ -52,6 +58,7 @@ public class Libro {
 		this.precio = precio;
 	}
 
+	// Sobreescribimos el método toString para que nos imprima el objeto libro con todos sus atributos y su valor
 	@Override
 	public String toString() {
 		return "libro [isbn=" + isbn + ", autor=" + autor + ", titulo=" + titulo + ", precio=" + precio + "]";
